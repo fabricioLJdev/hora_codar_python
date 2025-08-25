cf0bf2189f1ade203b608f757cbd8fed2a455c9c
fabricioLJdev <fabricioj516gb@outlook.com>
2025-08-25 14:36:11 -0300

    organizando pasta no github

--- 2_SINTAXE_E_ESTRUTURA/3_exercicio.py
+++ 2_SINTAXE_E_ESTRUTURA/3_exercicio.py
@@ -1,44 +1,44 @@
-# ENUNCIADOS E DESCRIÇÕES
+# # ENUNCIADOS E DESCRIÇÕES
 
-# Exercício 1:
-# Enunciado: Crie um programa que exiba a mensagem "Olá, Mundo!" na tela.
-# Descrição: O programa deve utilizar a função print() para exibir a mensagem
-# "Olá, Mundo!" no console. Este exercício reforça o uso básico da função
-# print() e a estrutura simples de um programa Python.
+# # Exercício 1:
+# # Enunciado: Crie um programa que exiba a mensagem "Olá, Mundo!" na tela.
+# # Descrição: O programa deve utilizar a função print() para exibir a mensagem
+# # "Olá, Mundo!" no console. Este exercício reforça o uso básico da função
+# # print() e a estrutura simples de um programa Python.
 
-# Exercício 2:
-# Enunciado: Escreva um programa que solicite o nome do usuário e exiba uma
-# saudação personalizada.
-# Descrição: O programa deve usar a função input() para receber o nome do
-# usuário e armazená-lo em uma variável. Em seguida, deve exibir uma mensagem
-# de saudação que inclua o nome fornecido, utilizando a função print().
+# # Exercício 2:
+# # Enunciado: Escreva um programa que solicite o nome do usuário e exiba uma
+# # saudação personalizada.
+# # Descrição: O programa deve usar a função input() para receber o nome do
+# # usuário e armazená-lo em uma variável. Em seguida, deve exibir uma mensagem
+# # de saudação que inclua o nome fornecido, utilizando a função print().
 
-# Exercício 3:
-# Enunciado: Desenvolva um programa que demonstra o uso correto da indentação
-# em uma estrutura condicional.
-# Descrição: O programa deve solicitar um número ao usuário e verificar se ele
-# é positivo, negativo ou zero. Utilize estruturas condicionais com a
-# indentação adequada para determinar e exibir o resultado.
+# # Exercício 3:
+# # Enunciado: Desenvolva um programa que demonstra o uso correto da indentação
+# # em uma estrutura condicional.
+# # Descrição: O programa deve solicitar um número ao usuário e verificar se ele
+# # é positivo, negativo ou zero. Utilize estruturas condicionais com a
+# # indentação adequada para determinar e exibir o resultado.
 
-# Exercício 4:
-# Enunciado: Crie um programa que solicita dois números ao usuário e exibe a
-# soma deles.
-# Descrição: O programa deve utilizar a função input() para receber dois
-# números, converter as entradas para o tipo numérico apropriado float(), calcular a
-# soma e exibir o resultado utilizando print().
+# # Exercício 4:
+# # Enunciado: Crie um programa que solicita dois números ao usuário e exibe a
+# # soma deles.
+# # Descrição: O programa deve utilizar a função input() para receber dois
+# # números, converter as entradas para o tipo numérico apropriado float(), calcular a
+# # soma e exibir o resultado utilizando print().
 
-# Exercício 5:
-# Enunciado: Escreva um programa que calcula a idade do usuário com base no ano
-# de nascimento fornecido.
-# Descrição: O programa deve solicitar o ano de nascimento do usuário, converter
-# a entrada para inteiro, calcular a idade atual considerando o ano atual e
-# exibir a idade utilizando print(). Utilize comentários para explicar cada
-# etapa do código.
+# # Exercício 5:
+# # Enunciado: Escreva um programa que calcula a idade do usuário com base no ano
+# # de nascimento fornecido.
+# # Descrição: O programa deve solicitar o ano de nascimento do usuário, converter
+# # a entrada para inteiro, calcular a idade atual considerando o ano atual e
+# # exibir a idade utilizando print(). Utilize comentários para explicar cada
+# # etapa do código.
 
 # EX1
 print("Olá, Mundo!")
 
-# EX2
+# EX2 
 nome = input("Olá, tudo bem? Qual é o seu nome?")
 
 print(f"Bem vindo {nome} a uns dos meus primeiros projetos em python!")
